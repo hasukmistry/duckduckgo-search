7743dc020097e8a0f86c3d6785a4903f8c8b02ed
Hasmukh K Mistry <hasmukhmistry137@gmail.com>
2024-03-12 18:21:43 +0530

    Scripts and configs adjustments to deploy app with vercel

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
 import app from './app/index.js';
 
-const port = process.env.PORT || 3000;
+if (process.env.NODE_ENV === 'development') {
+  const port = process.env.PORT || 3000;
 
-// Start the server
-app.listen(port, () => {
-  // eslint-disable-next-line no-console
-  console.log(`Server running on port ${port}`);
-});
+  // Start the server
+  app.listen(port, () => {
+    // eslint-disable-next-line no-console
+    console.log(`Server running on port ${port}`);
+  });
+}
+
+// We need to export our app from the file instead of starting the server with app.listen().
+// So, Express.js application will work as a serverless function with Vercel.
+// Vercel will handle the server part for us.
+export default app;
