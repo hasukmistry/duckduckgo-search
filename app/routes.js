c95f6254094009eb6356b12d3f13fa20e7dd8c9c
Hasmukh K Mistry <hasmukhmistry137@gmail.com>
2024-03-12 18:27:23 +0530

    Add index route to application

--- app/routes.js
+++ app/routes.js
@@ -4,6 +4,10 @@ import SearchController from './controllers/SearchController.js';
 const router = express.Router();
 
 // Defined routes
+router.get('/', (req, res) => {
+  res.json({ message: 'Welcome to search API' });
+});
+
 router.get('/search/:site/:query', SearchController.get.bind(SearchController));
 
 // TODO: convert get routes to post route
